747d97f4376518be3fd65d491d9ec6a039461b0e
MatApple <mat@mesosphere.io>
2016-11-13 09:55:04 -0800

    Adding jest __mocks__ to endpoints

---
+++ graphql/api/__mocks__/models.js
@@ -0,0 +1,23 @@
+import endpointsConnector from '../data/EndpointsConnector';
+import createStore from '../store';
+
+import Agents from './agents/models';
+import Frameworks from './frameworks/models';
+import Groups from './groups/models';
+import Tasks from './tasks/models';
+
+export default function models(mockEndpointData) {
+  const endpoints = endpointsConnector(mockEndpointData);
+  // Store is where we process and cache data like e.g. merged from marathon
+  // and mesos. The store allows us to lazily process the data only once
+  // for optimized lookups and minimal processing and is accessible by all
+  // resolvers.
+  const store = createStore(endpoints);
+
+  return {
+    Agents: new Agents({ store }),
+    Frameworks: new Frameworks({ store }),
+    Groups: new Groups({ store }),
+    Tasks: new Tasks({ store })
+  };
+}
