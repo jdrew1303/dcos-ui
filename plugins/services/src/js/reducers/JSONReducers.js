daff97112fb26e8419146d77ad27d409effddd11
mlunoe <michael.lunoe@gmail.com>
2016-11-18 12:12:05 -0800

    Rename JSONParserReducers.js -> JSONParser and JSONConfigReducers.js -> JSONReducers

--- plugins/services/src/js/reducers/JSONReducers.js
+++ plugins/services/src/js/reducers/JSONReducers.js
@@ -1,6 +1,8 @@
 import {JSONReducer as container} from './serviceForm/Container';
+import {JSONReducer as containers} from './serviceForm/Containers';
 import {JSONReducer as env} from './serviceForm/EnvironmentVariables';
 import {JSONReducer as labels} from './serviceForm/Labels';
+import {SET} from '../../../../../src/js/constants/TransactionTypes';
 import {
   simpleFloatReducer,
   simpleIntReducer,
@@ -11,10 +13,11 @@ module.exports = {
   id: simpleReducer('id', '/'),
   instances: simpleIntReducer('instances', 1),
   container,
+  containers,
   cpus: simpleFloatReducer('cpus', 0.01),
   mem: simpleIntReducer('mem', 128),
   disk: simpleIntReducer('disk', 0),
-  cmd: simpleReducer('cmd'),
+  cmd: simpleIntReducer('cmd'),
   env,
   labels
 };
